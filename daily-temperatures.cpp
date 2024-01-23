923d428d5d91e27da3f50a539c8b5a8951ed1f2c
akhi799 <74193911+akhi799@users.noreply.github.com>
2024-01-23 15:20:18 +0530

    Create daily-temperatures.cpp

---
+++ daily-temperatures.cpp
@@ -0,0 +1,30 @@
+//nearest greater to right
+//next greater element using stack
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> ans;
+        int n=temperatures.size();
+        for(int i=n-1;i>=0;i--){
+            if(st.size()==0){
+                ans.push_back(0);
+            }else if(st.size()>0&&temperatures[st.top()]>temperatures[i]){
+                ans.push_back(st.top()-i);
+            }else if(st.size()>0&&temperatures[st.top()]<=temperatures[i]){
+                while(st.size()>0&&temperatures[st.top()]<=temperatures[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    ans.push_back(0);
+                }
+                else{
+                    ans.push_back(st.top()-i);
+                }
+            }
+            st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
