a3dbe7159463b0ac57a299083c4882a72a412221
akhi799 <74193911+akhi799@users.noreply.github.com>
2024-01-24 10:12:33 +0530

    Create stock span problem.cpp

---
+++ stock span problem.cpp
@@ -0,0 +1,26 @@
+class StockSpanner {
+private:
+    stack<pair<int, int>> s;
+
+public:
+    StockSpanner() {
+    // public
+    //     StockSpanner() { this.stack = new Stack<>(); }
+    }
+
+    int next(int price) {
+        int span=1;
+        while(!s.empty()&&s.top().first<=price){
+            span+=s.top().second;
+            s.pop();
+        }
+        s.push({price,span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
